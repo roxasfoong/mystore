6abf4b4dd0c0bae0e3c16438393869eeceba1704
Roxas Foong <roxas.foong@gmail.com>
2024-04-12 15:59:06 +0800

    Install & Implement Basic Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
@@ -11,7 +11,8 @@ const App = () => {
 
     <Container>
     <h1>Welcome to My Store</h1>
-    <HomeScreen/>
+    {/* <HomeScreen/> */}
+    <Outlet />
     </Container>
 
     </main>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer }  from 'react-router-bootstrap';
 import logo from '../assets/logo.png';
 
 const Header = () => {
@@ -8,15 +9,21 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
+          <LinkContainer to='/'>
           <Navbar.Brand href="/">
             <img src={logo} alt='MyStire'/>
             MyStore
             </Navbar.Brand>
+            </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
+            <LinkContainer to='/cart'>
               <Nav.Link href="/cart"><FaShoppingCart />Cart</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
               <Nav.Link href="/signin"><FaUser />Sign In</Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
