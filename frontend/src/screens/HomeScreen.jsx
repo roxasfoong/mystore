7c1ca80e8cda97434a95a95c2f61cc0461b07043
Roxas Foong <roxas.foong@gmail.com>
2024-04-18 19:24:52 +0800

    Install Axios and fetch data from backend api from server.js

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react';
 import {Row, Col} from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../products';
+import axios from 'axios';
+
+//import products from '../products';
 
 
 const HomeScreen = () => {
+
+  const [products,setProducts] = useState([]);
+
+  useEffect( () => {
+
+    const fetchProducts = async () => {
+
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+
+    };
+    
+    fetchProducts();
+
+  }, []);
+
+
   return (
     <>
 
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,20 +1,42 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Row,Col,Image,ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from "../products";
+import axios from 'axios';
+//import products from "../products";
 
 
 const ProductScreen = () => {
 
+    const [product, setProduct] = useState([]);
+
     const { id:productId } = useParams();
-    const product = products.find((p) => p._id === productId );
+
+    useEffect(() => {
+
+        // Define an asynchronous function 'fetchProducts' to fetch data from the backend
+        const fetchProduct = async () => {
+          
+          // Make an HTTP GET request to the '/api/products' endpoint using Axios
+          const { data } = await axios.get(`/api/products/${productId}`);
+          
+          // Update the 'products' state with the data fetched from the endpoint
+          setProduct(data);
+        };
+        
+        // Call the 'fetchProducts' function when the component mounts (empty dependency array)
+        fetchProduct();
+    
+      }, [productId]);
+
+    //const product = products.find((p) => p._id === productId );
 
     /* console.log(product); */
 
   return <>
 
-  
+   
     <Link className='btn btn-light my-3' to='/'>
         Back
     </Link>
